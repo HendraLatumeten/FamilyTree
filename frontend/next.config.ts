75c1777abb4f74f7fcca6f19b074bda435ac7593
Hendra-Ikon <hendra.latumeten@ikonsultan.com>
2026-04-04 10:55:39 +0700

    update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   devIndicators: {
     appIsrStatus: false,
   },
+  experimental: {
+    allowedDevOrigins: ['43.157.235.159'],
+  },
 };
 
 export default nextConfig;
